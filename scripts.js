a8be0c49a21e2da9c3a1525d07d8ee804b9b555b
CocoNixy <m.nicole1994@gmail.com>
2022-11-09 18:03:51 +0200

    JS intro

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// console.log("Hello world")
+// console.error("oj")
+// // alert("Sveiki!")
+
+// // text is variable. gali buti kiti zodziai
+// // let nerasomas, kai deklaravimas jau sukurtas
+// let text1 = "Hello world";
+// console.log(text);
+
+// let text = "Hello world"; //naujas kintamasis
+// text = "Hello class"; //senas kintamasis, bet pakeiciama o reiksme
+
+// let tekstasA; // naujas kintamasis (deklaruojamas) // undefined
+// let tekstasB = "Hello world";
+
+// console.log(tekstasA); //undefined
+// console.log(tekstasB); //Hello world
+
+
+let pirmasSkaicius = 123456789;
+let antrasSkaicius = 5;
+let rezultatas = pirmasSkaicius + antrasSkaicius;
+
+console.log(rezultatas);
+antrasSkaicius = 0
+console.log(pirmasSkaicius + antrasSkaicius);
+antrasSkaicius = undefined;
+console.log(pirmasSkaicius + antrasSkaicius);
